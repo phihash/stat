3a578897f8f3f11cf4c6a3b7cd57ad687ef6102f
phihash <insharp0220@gmail.com>
2024-05-05 00:43:09 +0900

    選択された都道府県を配列で管理する関数(handleCheckboxChange)を作成

--- components/prefectures.tsx
+++ components/prefectures.tsx
@@ -6,11 +6,22 @@ interface Prefecture {
 }
 
 const Prefectures = () => {
-  const [prefectures, setPrefectures] = useState<Prefecture[]>()
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([])
+  const [selectedPrefectures, setSelectedPrefectures] = useState<number[]>([])
   async function getData() {
     const res = await fetch('/api/resas')
     const data = await res.json()
     setPrefectures(data.data.result)
+  }
+
+  const handleCheckboxChange = (prefCode: number) => {
+    setSelectedPrefectures((prevSelected) => {
+      if (prevSelected.includes(prefCode)) {
+        return prevSelected.filter((code) => code !== prefCode)
+      } else {
+        return [...prevSelected, prefCode]
+      }
+    })
   }
 
   useEffect(() => {
@@ -19,11 +30,17 @@ const Prefectures = () => {
 
   return (
     <div>
-      {prefectures ? (
+      {prefectures.length > 0 ? (
         <ul>
+          {selectedPrefectures}
           {prefectures.map((prefecture) => (
             <div key={prefecture.prefCode} className="flex">
-              <input type="checkbox" name="" id={prefecture.prefName} />
+              <input
+                type="checkbox"
+                name={prefecture.prefName}
+                id={prefecture.prefName}
+                onChange={() => handleCheckboxChange(prefecture.prefCode)}
+              />
               <label htmlFor={prefecture.prefName}>{prefecture.prefName}</label>
             </div>
           ))}
